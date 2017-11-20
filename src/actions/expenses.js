4b89fed257e7ef327bc6bc6df49136a692d94130
ksogata <ksogata@localhost.localdomain>
2017-11-19 22:44:18 -0800

    setup startRemoveExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -33,6 +33,14 @@ export const removeExpense = ({ id } = {}) => ({
   id
 });
 
+export const startRemoveExpense = ({ id } = {}) => {
+  return (dispatch) => {
+    return database.ref('expenses').child(id).remove().then(() => {
+      dispatch(removeExpense({ id }));
+    });
+  }
+};
+
 // EDIT_EXPENSE
 export const editExpense = ({ id, updates = {} } = {}) => ({
   type: 'EDIT_EXPENSE',
