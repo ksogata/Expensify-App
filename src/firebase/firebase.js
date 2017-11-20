babdac3e720e8b82b150ac9c4d54dd86f93ac4b1
ksogata <ksogata@localhost.localdomain>
2017-11-19 16:14:59 -0800

    setup test database env

---
+++ src/firebase/firebase.js
@@ -0,0 +1,74 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+export { firebase, database as default };
+
+// database.ref('expenses').push({
+//   description: 'some description',
+//   note: 'some note',
+//   amount: 234,
+//   createdAt: 0
+// });
+
+// database.ref('expenses').on('value', (snapshot) => {
+//   const expenses = [];
+//
+//   snapshot.forEach((childSnapshot) => {
+//     expenses.push({
+//       id: childSnapshot.key,
+//       ...childSnapshot.val()
+//     });
+//   });
+//
+//   console.log(expenses);
+// });
+
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+//
+// database.ref('expenses').on('child_added', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+
+
+// database.ref()
+//   .once('value')
+//   .then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+//   })
+//   .catch((e) => {
+//     console.log('error', e);
+//   });
+
+// database.ref().on('value', (snapshot) => {
+//   const val = snapshot.val();
+//   console.log(`${val.name} is a ${val.job.title} at ${val.job.company }`)
+// })
+// database.ref().set({
+//   name: 'Andrew Mead',
+//   age: 26,
+//   stressLevel: 8,
+//   job: {
+//     title: 'Software Developer',
+//     company: 'Google'
+//   },
+//   location: {
+//     city: 'Mountain View',
+//     country: 'United States'
+//   }
+// })
