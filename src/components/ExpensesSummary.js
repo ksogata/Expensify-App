5e06949a2fdeb3843df9307ab8c545d7f2a40116
ksogata <ksogata@localhost.localdomain>
2017-12-11 20:03:15 -0800

    Add Expensify styles

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import numeral from 'numeral';
 import selectExpensesTotal from '../selectors/expenses-total';
 import selectExpenses from '../selectors/expenses';
@@ -9,9 +10,16 @@ export const ExpensesSummary = ({ expensesCount, expensesTotal }) => {
   const formattedExpensesTotal = numeral(expensesTotal/100).format('$0,0.00');
 
   return (
-    <h1>
-      Viewing {expensesCount} {expensesWord} totalling {formattedExpensesTotal}
-    </h1>
+    <div className="page-header">
+      <div className="content-container">
+      <h1 className="page-header__title">
+        Viewing <span>{expensesCount}</span> {expensesWord} totalling <span>{formattedExpensesTotal}</span>
+      </h1>
+      <div className="page-header__actions">
+        <Link className="button" to="/create">Add Expense</Link>
+      </div>
+    </div>
+  </div>
   );
 };
 
